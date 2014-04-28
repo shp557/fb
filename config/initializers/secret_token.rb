91bcae9bd18e058abc21589a5305c7dfaf3fff77
Sachin Patel <sachb52@gmail.com>
2014-04-28 13:23:15 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fb::Application.config.secret_key_base = 'cc7ff6d1856ffbb572ec0cdfe5d5020792422ab308f3757a1cbf744f349d10f31a83fc6fba158e12fdb1269be80ab3a7107898ee1f65410d946747d30dfe9ebe'
